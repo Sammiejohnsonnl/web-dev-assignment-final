4455dca72d44b6ca4c041b22090439fcdda37c77
Sammiejohnsonnl <c3055721@hallam.shu.ac.uk>
2024-01-24 08:32:56 +0100

    w

--- styles/script.js
+++ styles/script.js
@@ -18,3 +18,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('C:\Users\SJ\Desktop\webdev-final\web-dev-assignment-final')
+        .then(response => response.json())
+        .then(data => {
+            const coursesSection = document.querySelector('.courses-section');
+
+            data.courses.forEach(course => {
+                const courseContainer = document.createElement('div');
+                courseContainer.classList.add('course');
+
+                const courseTitle = document.createElement('h2');
+                courseTitle.textContent = course.courseTitle;
+                const courseSummary = document.createElement('p');
+                courseSummary.textContent = course.courseSummary;
+                courseContainer.appendChild(courseTitle);
+                courseContainer.appendChild(courseSummary);
+                coursesSection.appendChild(courseContainer);
+            });
+        })
+        .catch(error => console.error('Error fetching courses data:', error));
+});
+
