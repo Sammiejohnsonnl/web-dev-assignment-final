e2fd12a2d1e5ef1ed2087949f22fd68c383f66d1
Sammiejohnsonnl <c3055721@hallam.shu.ac.uk>
2024-01-24 07:16:19 +0100

    w

--- styles/script.js
+++ styles/script.js
@@ -4,3 +4,17 @@ function toggleNav() {
         navList.classList.toggle('active');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const currentUrl = window.location.href;
+
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        const linkUrl = link.href;
+
+        if (currentUrl === linkUrl) {
+            link.parentElement.classList.add('active');
+        }
+    });
+});
